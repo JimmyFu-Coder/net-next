5661214993aa0c54621e72665da5c45ca96d4dbf
jimmyRaptor <jimmy.fu@raptortech.com.au>
2025-02-07 23:22:25 +0800

    remove unused files

--- Carsties/src/AuctionService/obj/Debug/net9.0/AuctionService.AssemblyInfo.cs
+++ Carsties/src/AuctionService/obj/Debug/net9.0/AuctionService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aacef38069b2852dc4bbd88045a911a9324785f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b15211ad6a86f10273520dcb1c78d174ffcaac0")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
