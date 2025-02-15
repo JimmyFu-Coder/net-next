e5d5fb93871e0b5ffb01cdaaa7d3984673bca431
jimmyRaptor <jimmy.fu@raptortech.com.au>
2025-02-15 16:30:52 +0800

    add git ignore and controller

---
+++ Carsties/src/AuctionService/Controllers/AuctionsController.cs
@@ -0,0 +1,16 @@
+﻿using AuctionService.Data;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuctionService.Controllers;
+
+[ApiController]
+[Route("api/auctions")]
+public class AuctionsController: ControllerBase
+{
+    private readonly AuctionDbContext context;
+    public AuctionsController(AuctionDbContext context, IMapper mapper)
+    {
+        this.context = context;
+    }
+}
